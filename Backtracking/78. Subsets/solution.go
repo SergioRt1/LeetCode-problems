301363b4d73c738e23766fd54c1d0760fc07c9fd
SergioRt1 <sergio200035@gmail.com>
2023-06-29 02:13:49 -0500

    78. Subsets

---
+++ Backtracking/78. Subsets/solution.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func subsets(nums []int) [][]int {
+	return rec(nums, 0)
+}
+
+// Recursive solution with arrays
+func rec(nums []int, idx int) [][]int {
+	response := make([][]int, 0)
+	if idx == len(nums) {
+		return append(response, []int{})
+	}
+
+	for _, elem := range rec(nums, idx+1) {
+		include := make([]int, len(elem))
+		copy(include, elem)
+		include = append(include, nums[idx])
+		response = append(response, elem, include)
+	}
+
+	return response
+}
+
+// Use bit set to decode groups
+func bits(nums []int, idx int) []int {
+	if idx == len(nums) {
+		return []int{0}
+	}
+	response := make([]int, 0)
+	for _, elem := range bits(nums, idx+1) {
+		include := 1<<(nums[idx]+10) | elem
+		response = append(response, elem, include)
+	}
+
+	return response
+}
+
+func decode(nums []int) [][]int {
+	response := make([][]int, 0, len(nums))
+	for _, n := range nums {
+		base := -10
+		set := make([]int, 0)
+		for n > 0 {
+			if n&1 == 1 {
+				set = append(set, base)
+			}
+			base++
+			n >>= 1
+		}
+		response = append(response, set)
+	}
+	return response
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(subsets(nums))
+	fmt.Println(bits(nums, 0))
+	fmt.Println(decode(bits(nums, 0)))
+}
